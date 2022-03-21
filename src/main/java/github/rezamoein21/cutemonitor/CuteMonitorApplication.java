7db497b095c55237a1e5f3b5314db5038604dd61
reza_moein <rezamoeinpe@gmail.com>
2022-03-21 16:28:26 +0330

    Initial setup of application

---
+++ src/main/java/github/rezamoein21/cutemonitor/CuteMonitorApplication.java
@@ -0,0 +1,13 @@
+package github.rezamoein21.cutemonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuteMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CuteMonitorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/github/rezamoein21/cutemonitor/CuteMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package github.rezamoein21.cutemonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CuteMonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
