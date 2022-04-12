fdc75151cec3ec23547603cac176424a035dcde9
reza_moein <rezamoeinpe@gmail.com>
2022-04-12 09:40:23 +0430

    Change architecture of project
    add document
    remove unused entity and tests

--- src/main/java/github/rezamoein21/cutemonitor/CuteMonitorApplication.java
+++ src/main/java/github/rezamoein21/cutemonitor/CuteMonitorApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CuteMonitorApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CuteMonitorApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CuteMonitorApplication.class, args);
+    }
 
 }
