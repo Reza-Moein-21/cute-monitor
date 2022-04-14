58be992e3949dcf91a644116fccda377ff1068cb
reza_moein <rezamoeinpe@gmail.com>
2022-04-14 11:56:34 +0430

    Arrange project in separate module

--- api/src/main/java/github/rezamoein21/cutemonitor/api/CuteMonitorApplication.java
+++ api/src/main/java/github/rezamoein21/cutemonitor/api/CuteMonitorApplication.java
@@ -1,4 +1,4 @@
-package github.rezamoein21.cutemonitor;
+package github.rezamoein21.cutemonitor.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/github/rezamoein21/cutemonitor/enums/ExecutionStatus.java
+++
@@ -1,5 +0,0 @@
-package github.rezamoein21.cutemonitor.enums;
-
-public enum ExecutionStatus {
-    STOPPED, RUNNING
-}
